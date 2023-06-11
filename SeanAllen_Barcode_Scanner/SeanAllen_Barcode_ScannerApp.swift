e2c8accae4e4ee76e018b09550c6b2f5bbdf5a8e
Batson Seales <batson.seales@gmail.com>
2023-06-11 18:26:42 -0500

    UI structure.

--- SeanAllen_Barcode_Scanner/SeanAllen_Barcode_ScannerApp.swift
+++ SeanAllen_Barcode_Scanner/SeanAllen_Barcode_ScannerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SeanAllen_Barcode_ScannerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
