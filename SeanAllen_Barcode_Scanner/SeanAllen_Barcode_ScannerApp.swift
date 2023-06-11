4edd349e00bd005f88ded1a88480efc27fc5150d
Batson Seales <batson.seales@gmail.com>
2023-06-11 18:03:13 -0500

    Initial Commit

---
+++ SeanAllen_Barcode_Scanner/SeanAllen_Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeanAllen_Barcode_ScannerApp.swift
+//  SeanAllen_Barcode_Scanner
+//
+//  Created by Batson Seales on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SeanAllen_Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
